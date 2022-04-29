009aca113fc9a0e2f4fcceb5ffda2fd78ea2a834
NAME <email>
2022-04-29 12:38:55 +0530

    beanscope

---
+++ 01-springcore/src/main/java/com/springdemos/springcore/beanscopes/Test.java
@@ -0,0 +1,26 @@
+package com.springdemos.springcore.beanscopes;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		// create the spring container - box
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/springdemos/springcore/beanscopes/config.xml");
+
+		// get the hospital bean from the container
+		Address address = (Address) context.getBean("address");
+		
+		// print the bean
+		System.out.println(address);
+		System.out.println("1st address hashCode" +address.hashCode());
+		
+		
+		Address address2 = (Address) context.getBean("address");
+		System.out.println("2nd address hashCode" +address2.hashCode());
+		//for same hashcode change scope to 'singleton' in config.xml
+	}
+
+}
